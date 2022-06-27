15375e00ac6e3888d248f7d3f37de61b38980733
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-27 15:11:26 +0200

    creation include

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once './includes/header.php';
+require_once './includes/main.php';
+require_once './includes/footer.php';
+// _once = ça le fait qu'une fois 
+// require = pareil mais stricte une erreur et plus rien marche include meme si erreur ça passe
