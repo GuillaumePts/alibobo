22bdae6c25f6dd6ad7f82a1a6214e2b1c6fc56af
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-04 09:56:54 +0200

    creation dossier et fichier function

---
+++ function/function.php
@@ -0,0 +1,8 @@
+<?php
+
+function dump($x){
+    echo  "<pre>";
+ var_dump($x);
+echo  "</pre>";
+ 
+}
--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 // pour determiner notre fuseau horaire = date_default_timezone_get() 
 date_default_timezone_set('Europe/Paris');
 
-
+require_once './function/function.php';
 require_once './includes/header.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
 // _once = ça le fait qu'une fois 
 // require = pareil mais stricte une erreur et plus rien marche include meme si erreur ça passe
 
+echo $_SERVER["SERVER_SOFTWARE"];
+
+dump($_SERVER);
