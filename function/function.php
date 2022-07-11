4b6f09e5226fd2cc44a08f926f0f797c09f08d40
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-11 10:31:08 +0200

    premier commit inscription

--- function/function.php
+++ function/function.php
@@ -5,4 +5,48 @@ function dump($x){
  var_dump($x);
 echo  "</pre>";
  
+};
+
+
+function autoInclude(string $file): void {
+    $includedFiles = glob("./includes/*.inc.php");
+    $file = "./includes/" . $file . ".inc.php";
+
+    if (count($includedFiles) !== 0 && in_array($file, $includedFiles)) {
+        require_once $file;
+    } else {
+        require_once './includes/accueil.inc.php';
+    }
+}
+
+
+function validText($er, $data, $key, $min, $max)
+{
+    if(!empty($data)) {
+        if(mb_strlen($data) < $min) {
+            $er[$key] = 'min '.$min.' caractères';
+        } elseif(mb_strlen($data) >= $max) {
+            $er[$key] = 'max '.$max.' caractères';
+        }
+    } else{
+        $er[$key] = 'Veuillez renseigner ce champ';
+    }
+    return $er;
+}
+
+
+function getValue($key,$data = null){
+    if(!empty($_POST[$key])) {
+        return $_POST[$key];
+    } else {
+        if(!empty($data)) {
+            return $data;
+        }
+    }
+    return '';
+}
+
+function cleanXss(string $key)
+{
+    return trim(strip_tags($_POST[$key]));
 }
--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 // pour determiner notre fuseau horaire = date_default_timezone_get() 
 date_default_timezone_set('Europe/Paris');
 
-require_once './function/function.php';
-require_once './includes/header.php';
-require_once './includes/main.php';
-require_once './includes/footer.php';
+require './function/function.php';
+require './includes/header.php';
+require './includes/main.php';
+require './includes/footer.php';
 // _once = ça le fait qu'une fois 
 // require = pareil mais stricte une erreur et plus rien marche include meme si erreur ça passe
 
 echo $_SERVER["SERVER_SOFTWARE"];
 
-dump($_SERVER);
+// dump($_SERVER);
