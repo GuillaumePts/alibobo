0557f91122cee7df02b4f17fd184c4a61f762e07
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-18 10:29:40 +0200

    test de dump

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 require_once './functions/autoLoad.php';
 autoLoad("*.php");
 
+require __DIR__.'/vendor/autoload.php';
+$toto ="il fait bon";
+dump($toto);
+
 // Définir le fuseau horaire dans lequel le serveur se trouve
 date_default_timezone_set('Europe/Paris');
 
@@ -10,6 +14,8 @@ date_default_timezone_set('Europe/Paris');
 * include renvoie un avertissement simple en cas d'erreur
 * require renvoie une erreur fatale et arrête l'exécution du script
 */
+
+
 require_once './includes/header.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
